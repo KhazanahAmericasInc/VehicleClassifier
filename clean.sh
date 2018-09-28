c7ecc20b1471d5bfe93a9bb57676bf5be16d3823
vincenthang <gghvince@gmail.com>
2018-09-28 16:51:45 -0700

    added directory cleaning bash script

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+#
+# This script cleans all of the training data and
+# scraped images from the directory.
+#
+#
+
+rm -Rf scraped_images
+rm -Rf resized2
+rm -Rf resized3
+rm classhistogram.png datasummary.txt imagelist.csv
