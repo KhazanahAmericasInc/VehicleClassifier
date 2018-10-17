023d295cc3a3a5778317ce2f9c4962df0aaec02a
vincenthang <gghvince@gmail.com>
2018-10-17 14:24:02 -0700

    updated scrape_default.sh

--- scrape_default.sh
+++ scrape_default.sh
@@ -13,3 +13,7 @@ python3 scrape_image.py
 python3 image_list.py
 python3 image_noise.py
 mv resized3 resized
+python3 image_list_resized.py
+python3 create_data.py
+python3 train_model.py --learning_rate 0.001
+python3 remove_dropout.py
