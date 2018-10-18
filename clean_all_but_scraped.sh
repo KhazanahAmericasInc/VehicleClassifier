7c120a99b4dde638a95a808e70b28822f9c241cc
vincenthang <gghvince@gmail.com>
2018-10-18 11:32:08 -0700

    created a clean script for everything except scraped_images.py

---
+++ clean_all_but_scraped.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+#
+# This bash script cleans all files except
+# scraped_images
+#
+#
+
+rm -Rf resized2
+rm -Rf resized3
+rm -Rf output
+rm -Rf resized
+
+rm classhistogram.png datasummary.txt imagelist.csv nodropout.pb data.p
